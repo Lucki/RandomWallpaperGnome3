ccb4f9ccc6f9b43859cdfa547a41c07dbe95f8bb
Lucki <Lucki@holarse.de>
2022-11-20 19:31:35 +0100

    Only use one settings module and use enums again

--- randomwallpaper@iflow.space/settings.js
+++ randomwallpaper@iflow.space/settings.js
@@ -3,6 +3,18 @@ const Gio = imports.gi.Gio;
 const Self = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Self.imports.convenience;
 
+var RWG_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.space.iflow.randomwallpaper';
+var RWG_SETTINGS_SCHEMA_BACKEND_CONNECTION = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.backend-connection';
+var RWG_SETTINGS_SCHEMA_SOURCES_GENERAL = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.general';
+var RWG_SETTINGS_SCHEMA_SOURCES_GENERIC_JSON = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.genericJSON';
+var RWG_SETTINGS_SCHEMA_SOURCES_LOCAL_FOLDER = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.localFolder';
+var RWG_SETTINGS_SCHEMA_SOURCES_REDDIT = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.reddit';
+var RWG_SETTINGS_SCHEMA_SOURCES_UNSPLASH = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.unsplash';
+var RWG_SETTINGS_SCHEMA_SOURCES_URL_SOURCE = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.urlSource';
+var RWG_SETTINGS_SCHEMA_SOURCES_WALLHAVEN = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.wallhaven';
+
+var RWG_SETTINGS_SCHEMA_PATH = `/org/gnome/shell/extensions/space-iflow-randomwallpaper`;
+
 var Settings = class {
 
 	/**
@@ -15,12 +27,28 @@ var Settings = class {
 		this._settings = Convenience.getSettings(schema, path);
 	}
 
-	observe(key, callback) {
-		return this._settings.connect('changed::' + key, callback);
+	bind(keyName, gObject, property, settingsBindFlags) {
+		this._settings.bind(keyName, gObject, property, settingsBindFlags);
 	}
 
 	disconnect(handler) {
 		return this._settings.disconnect(handler);
+	}
+
+	get(key, type) {
+		return this._settings['get_' + type](key);
+	}
+
+	getSchema() {
+		return this._settings.settings_schema;
+	}
+
+	observe(key, callback) {
+		return this._settings.connect('changed::' + key, callback);
+	}
+
+	reset(keyName) {
+		this._settings.reset(keyName);
 	}
 
 	set(key, type, value) {
@@ -30,9 +58,4 @@ var Settings = class {
 			throw "Could not set " + key + " (type: " + type + ") with the value " + value;
 		}
 	}
-
-	get(key, type) {
-		return this._settings['get_' + type](key);
-	}
-
 };
